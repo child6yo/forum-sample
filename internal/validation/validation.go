de782abed2acfc5353c60b0661881cf8d895b084
Arseniy Glushkov <miguelprod8@gmail.com>
2025-01-29 19:54:58 +0700

    feat: validation library

---
+++ internal/validation/validation.go
@@ -0,0 +1,159 @@
+package validation
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+)
+
+// Validate struct holds custom validation rules
+type Validate struct {
+	customRules map[string]func(value string, field reflect.Value) error
+}
+
+// ValidateStruct validates all fields of a struct based on the 'validate' tags.
+// Takes in any struct and returns an error if validation fails.
+//
+// This function uses reflection to iterate over all fields of the struct
+// and validate them using the specified tags. If any field fails validation,
+// an error is returned immediately.
+func (v *Validate) ValidateStruct(s interface{}) error {
+	val := reflect.ValueOf(s)
+	typ := reflect.TypeOf(s)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+		tag := fieldType.Tag.Get("validate")
+
+		if err := v.validateField(field, tag); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// validateField validates a single field based on the provided tag.
+// Takes in the field value and the validation tag, and returns an error if validation fails.
+//
+// The function parses the validation tag into individual rules and applies
+// these rules to the field value. It supports various types of validation
+// such as checking for valid username, email, maximum length, and minimum length.
+func (v *Validate) validateField(field reflect.Value, tag string) error {
+    if tag == "" {
+        return nil
+    }
+
+    rules := parseTag(tag)
+
+    for key, value := range rules {
+        switch key {
+        case "symbols":
+            if value == "username" && !isValidUsername(field.String()) {
+                return errors.New("invalid username")
+            }
+        case "email":
+            if !isValidEmail(field.String()) {
+                return errors.New("invalid email")
+            }
+        case "max_len":
+			maxLength := stringToInt(value)
+            if field.Len() > maxLength {
+                return fmt.Errorf("max length exceeded: field length is %d, max length is %d", field.Len(), maxLength)
+            }
+		case "min_len":
+			minLength := stringToInt(value)
+            if field.Len() < minLength {
+                return fmt.Errorf("min length not met: field length is %d, min length is %d", field.Len(), minLength)
+            }
+        }
+    }
+
+    return nil
+}
+
+// parseTag parses a validation tag and returns a map of rules.
+// Takes in a validation tag string, and returns a map where the keys are the rule names
+// and the values are the rule values.
+//
+// This function splits the tag string into key-value pairs based on commas and
+// equals signs. If a rule has no value, an empty string is used as the value.
+func parseTag(tag string) map[string]string {
+    rules := map[string]string{}
+    pairs := regexp.MustCompile(`,`).Split(tag, -1)
+
+    for _, pair := range pairs {
+        parts := regexp.MustCompile(`=`).Split(pair, 2)
+        if len(parts) == 2 {
+            rules[parts[0]] = parts[1]
+        } else {
+            rules[parts[0]] = ""
+        }
+    }
+
+    return rules
+}
+
+func isValidUsername(username string) bool {
+    re := regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+    return re.MatchString(username)
+}
+
+func isValidEmail(email string) bool {
+    re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+    return re.MatchString(email)
+}
+
+func stringToInt(s string) int {
+    value, _ := strconv.Atoi(s)
+    return value
+}
+
+
+// CustomValidateStruct validates all fields of a struct based on custom validation rules.
+// Takes in any struct and returns an error if validation fails.
+//
+// This function is similar to ValidateStruct but uses custom validation rules
+// specified in the 'c_validate' tags. If any field fails validation,
+// an error is returned immediately.
+func (v *Validate) CustomValidateStruct(s interface{}) error {
+	val := reflect.ValueOf(s)
+	typ := reflect.TypeOf(s)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+		tag := fieldType.Tag.Get("c_validate")
+
+		if err := v.customValidateField(field, tag); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// customValidateField validates a single field based on custom rules.
+// Takes in the field value and the validation tag, and returns an error if validation fails.
+//
+// The function parses the validation tag into individual custom rules and applies
+// these rules to the field value using custom validation functions defined in
+// the Validate struct.
+func (v *Validate) customValidateField(field reflect.Value, tag string) error {
+	if tag == "" {
+        return nil
+    }
+
+	rules := parseTag(tag)
+
+    for key, value := range rules {
+		if err := v.customRules[key](value, field); err != nil {
+			return fmt.Errorf("field %s validation failed: %s", value, err)
+		}
+	}
+
+	return nil
+}
---
+++ internal/validation/validation_test.go
@@ -0,0 +1,139 @@
+package validation
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestValidateStruct(t *testing.T) {
+	type User struct {
+		name  string `validate:"symbols=username,max_len=10,min_len=2"`
+		email string `validate:"email"`
+	}
+
+	testCases := []struct {
+		name    string
+		input   User
+		wantErr bool
+	}{
+		{
+			name:    "OK",
+			input:   User{name: "username", email: "username@gmail.com"},
+			wantErr: false,
+		},
+		{
+			name:    "Invalid symbols",
+			input:   User{name: "—u$$Rn@^^e", email: "username@gmail.com"},
+			wantErr: true,
+		},
+		{
+			name:    "Invalid max lenght",
+			input:   User{name: "coolusername", email: "username@gmail.com"},
+			wantErr: true,
+		},
+		{
+			name:    "Invalid min lenght",
+			input:   User{name: "u", email: "username@gmail.com"},
+			wantErr: true,
+		},
+		{
+			name:    "Invalid email",
+			input:   User{name: "username", email: "username"},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range testCases {
+		v := Validate{}
+		err := v.ValidateStruct(test.input)
+		if err == nil && test.wantErr {
+			t.Errorf("Test: %s failed. Unexpected error: %s", test.name, err)
+		}
+	}
+}
+
+func TestCustomStruct(t *testing.T) {
+	testCases := []struct {
+		name        string
+		input       interface{}
+		customTag   string
+		customRules map[string]func(value string, field reflect.Value) error
+		wantErr     bool
+	}{
+		{
+			name: "OK",
+			input: struct {
+				field string `c_validate:"work=true"`
+			}{field: "bebe"},
+			customRules: map[string]func(value string, field reflect.Value) error{
+				"work": func(value string, field reflect.Value) error {
+					if value != "true" {
+						return errors.New("work ist't true")
+					} else if field.String() != "bebe" {
+						return errors.New("string ist't bebe")
+					}
+					return nil
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid string value",
+			input: struct {
+				field string `c_validate:"work=false"`
+			}{field: "invalid"},
+			customRules: map[string]func(value string, field reflect.Value) error{
+				"work": func(value string, field reflect.Value) error {
+					if value != "true" {
+						return errors.New("work ist't true")
+					} else if field.String() != "bebe" {
+						return errors.New("string ist't bebe")
+					}
+					return nil
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Invalid tag value",
+			input: struct {
+				field string `c_validate:"work=false"`
+			}{field: "bebe"},
+			customRules: map[string]func(value string, field reflect.Value) error{
+				"work": func(value string, field reflect.Value) error {
+					if value != "true" {
+						return errors.New("work ist't true")
+					} else if field.String() != "bebe" {
+						return errors.New("string ist't bebe")
+					}
+					return nil
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name:  "No custom tag",
+			input: struct{ field string }{field: "bebe"},
+			customRules: map[string]func(value string, field reflect.Value) error{
+				"work": func(value string, field reflect.Value) error {
+					if value != "true" {
+						return errors.New("work ist't true")
+					} else if field.String() != "bebe" {
+						return errors.New("string ist't bebe")
+					}
+					return nil
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, test := range testCases {
+		v := Validate{customRules: test.customRules}
+		err := v.CustomValidateStruct(test.input)
+		if err == nil && test.wantErr {
+			t.Errorf("Test: %s failed. Unexpected error: %s", test.name, err)
+		}
+	}
+}
