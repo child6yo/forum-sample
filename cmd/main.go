795d971a1bcaf04200114b05c5828b5f942f1397
Arseniy Glushkov <miguelprod8@gmail.com>
2025-01-31 15:34:31 +0700

    feat: server starting

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/child6yo/forum-sample"
 	"github.com/joho/godotenv"
@@ -45,5 +48,24 @@ func main() {
 	handlers := handler.NewHandler(services)
 	
     srv := new(forum.Server)
-    srv.Run("8000", handlers.InitRoutes())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			log.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+	log.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Print("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Printf("error occured on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		log.Printf("error occured on db connection close: %s", err.Error())
+	}
 }
